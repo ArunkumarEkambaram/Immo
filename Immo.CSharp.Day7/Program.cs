4d77dddbe0f82a37f7011d1c73be65abfd06b8a0
Arunkumar <arunkumar.e@outlook.com>
2024-11-27 11:46:58 +0530

    Simple Singleton

--- Immo.CSharp.Day7/Program.cs
+++ Immo.CSharp.Day7/Program.cs
@@ -7,11 +7,17 @@ namespace Immo.CSharp.Day7
     {
         static void Main(string[] args)
         {
-            LoginSQL sql=new LoginSQL();
-            LoginOracle oracle=new LoginOracle();
-            FileLogger logger=new FileLogger();
-            LogErrorHandler logErrorHandler = new LogErrorHandler(sql);
-            logErrorHandler.DoWork("Runtime Error");
+            //LoginSQL sql=new LoginSQL();
+            //LoginOracle oracle=new LoginOracle();
+            //FileLogger logger=new FileLogger();
+            //LogErrorHandler logErrorHandler = new LogErrorHandler(sql);
+            //logErrorHandler.DoWork("Runtime Error");
+
+            SimpleSingleton obj1 = SimpleSingleton.Instance;
+            SimpleSingleton obj2 = SimpleSingleton.Instance;
+            SimpleSingleton obj3 = SimpleSingleton.Instance;
+
+            Console.WriteLine(SimpleSingleton.Counter);
         }
     }
 }
---
+++ Immo.CSharp.Day7/SimpleSingleton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Immo.CSharp.Day7
+{
+    /*
+     * Class Should be sealed
+     * Private Constructor
+     * Static Instance 
+     * Readonly property which return instance
+     * (Optional : Thread Safe)
+     * (Optional : Lazy Loading)
+     */
+    public sealed class SimpleSingleton
+    {
+        private static SimpleSingleton _instance = null;
+        private static readonly object _instanceLock = new object();
+        public static int Counter { get; set; }
+        private SimpleSingleton()
+        {
+        }
+
+        public static SimpleSingleton Instance
+        {
+            get
+            {
+                //Double Check Lock
+                if (_instance == null)
+                {
+                    //Thread Safe
+                    lock (_instanceLock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new SimpleSingleton();
+                            Counter++;
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
